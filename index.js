291076334c8acc822d8cdf79f3abfbc0546710d4
程杰文 <chengjiewen@chengjiwendeMBP.lan>
2020-11-29 21:36:56 +0800

    [feat]初始化项目

---
+++ index.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer');
+const program = require('commander');
+const createFtpClient = require('./lib/ftp');
+
+class ClientServer {
+    constructor () {
+        this.ftpClient = null;
+        this.questions = [
+            {
+                type: 'input',
+                name: 'host',
+                validate (value) {
+                    return value !== '' || '请输入服务器地址';
+                }
+            },
+            {
+                type: 'input',
+                name: 'port',
+                filter: Number,
+                validate (value) {
+                    return (value !== '' && typeof value === 'number') || '请输入端口号';
+                }
+            },
+            {
+                type: 'input',
+                name: 'user'
+            },
+            {
+                type: 'input',
+                name: 'password'
+            }
+        ]
+
+        this.init();
+    }
+
+    init () {
+        this.initCommand();
+    }
+
+    initCommand () {
+        program
+            .command('ftp-login')
+            .action(() => {
+                this.receiveConfig('ftp');
+            });
+
+        program
+            .command('sftp-login')
+            .action(() => {
+                this.receiveConfig('sftp');
+            });
+
+        program.parse(process.argv);
+    }
+
+    receiveConfig (type) {
+        inquirer
+            .prompt(this.questions)
+            .then(answers => {
+                let options = {
+                    type,
+                    config: answers
+                };
+
+                this.ftpClient = createFtpClient(options);
+            });
+    }
+}
+
+new ClientServer();
